14a1c3dc692e00465ff67f6ec64a29bdcc76c82a
michalpatryk <43810475+michalpatryk@users.noreply.github.com>
2020-11-07 18:20:37 +0100

    Basic routing_2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import AppRoutes from './App.routes'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+
 const App = () => {
     return(
         <div className="App">
             <Router>
                 <AppRoutes />
             </Router>
-            
         </div>
         
     )
