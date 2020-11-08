abe11f065954f57f6d6c75407046f0a50b5d6e24
Soku <ooo>
2020-11-08 19:43:25 +0100

    no dobra spierdalaj ja chce tylko checkout zrobic chuj ci w dupe linus torvalds

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import CategoriesList from './CategoriesList'
+import CategoriesList from './components/CategoriesList'
 
 ReactDOM.render(
   <React.StrictMode>
