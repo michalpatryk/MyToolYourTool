9d027766105df0a0957804125e18d4e1bb97b58b
michalpatryk <43810475+michalpatryk@users.noreply.github.com>
2020-11-11 12:57:58 +0100

    added app init

---
+++ src/components/App/App.init.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import Store from './App.store'
+
+const AppInit = () => {
+    const store = Store.useStore()
+    // useEffect(() => {
+    //     store.set('authToken')("002994da")
+    //     store.set(' userId')(0)
+    //     store.set('user')("123")
+    //     store.set('deviceToken')("asd")
+    // })
+    return null
+
+}
+export default AppInit
