0dbf2a0afd9f94187b9f967dca142ae32b7601ea
Soku <ooo>
2021-01-25 21:05:58 +0100

    nic nie dziala

--- src/components/App/App.routes.js
+++ src/components/App/App.routes.js
@@ -7,7 +7,8 @@ import {
   CategoryPage,
   MyAccountPage,
   SignUpPage,
-  SignInPage
+  SignInPage,
+  ItemListPage
 } from '../../pages'
 import Store from './App.store'
 
@@ -39,6 +40,10 @@ const AppRoutes = () => {
 
         <Route path="/categories">
           <CategoryPage/>
+        </Route>
+
+        <Route path="/items">
+          <ItemListPage />
         </Route>
 
         <Route path="/">
