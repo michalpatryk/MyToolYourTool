71e93bd71c2bd61a9dcb1a61d93013665398e265
Soku <ooo>
2021-01-25 21:25:12 +0100

    lista przedmiotow powinna dzialac, wymaga obslugi przez backend requesta /getitems?category={nazwa}

--- src/pages/ItemList/ItemList.js
+++ src/pages/ItemList/ItemList.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import queryString from 'query-string';
 
 import Header2 from '../../components/headers/Header2.js';
 import HorizontalNavLogCheck from '../../components/horizontal-navs/HorizontalNavLogCheck';
-import ItemList from '../../components/CategoriesList'
+import ItemList from '../../components/ItemList'
+import { useLocation } from 'react-router-dom';
 
-export default function Example() {
+export default function ItemListPage() {
+  const { search } = useLocation();
+  const queryParams = queryString.parse(search);
   return (
     <React.Fragment>
-       <HorizontalNavLogCheck
+      <HorizontalNavLogCheck
         content={{
           brand: {
             text: ' ',
@@ -19,11 +23,11 @@ export default function Example() {
           'link3': 'Sign in',
         }}
       />
-      <Header2 content={ {
+      <Header2 content={{
         'badge': 'My tool is your tool',
         'header': 'Exchange you tools',
-       } } />
-      <ItemList category='example'/>
+      }} />
+      <ItemList category={queryParams.category} />
     </React.Fragment>
   );
 }
