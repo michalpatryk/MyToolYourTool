60dabd5e82babe65ed1678f867f6f9b51616cf82
michalpatryk <43810475+michalpatryk@users.noreply.github.com>
2021-02-20 11:29:51 +0100

    Nowy widok myAccount

--- src/pages/MyAccount/MyAccount.js
+++ src/pages/MyAccount/MyAccount.js
@@ -13,13 +13,14 @@ import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import {VerticalNav1} from '../../components/vertical-navs/VerticalNav1';
 import Score1 from '../../components/features/Score1';
+import Table1 from '../../components/features/Table1';
 import Title1 from '../../components/features/Title1';
-import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const drawerWidth = 240;
 
@@ -98,7 +99,7 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
   },
   fixedHeight: {
-    height: 240,
+    height: 280,
   },
 }));
 
@@ -112,6 +113,12 @@ export default function Myaccount() {
     setOpen(false);
   };
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  const details = {
+    'name': 'Monika',
+    'surname': 'Kot',
+    'email': 'mmkotek@gmail.com',
+  };
 
   return (
     <div className={classes.root}>
@@ -147,24 +154,33 @@ export default function Myaccount() {
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* JAKIS ELEMENT ? pomysły*/}
+          <Grid container spacing={5}>
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>
-            
+              <Title1>My account details</Title1>
+              <br></br>
+              <Typography variant="subtitle1" gutterBottom={true}> Name : {details['name']}</Typography>
+              <Typography variant="subtitle1" gutterBottom={true}> Surname : {details['surname']}</Typography>
+              <Typography variant="subtitle1" gutterBottom={true}> Email : {details['email']}</Typography>
+     
+              <br></br>
+              <Grid container spacing={3}>
+              <Grid item xs={9}> </Grid>
+              <Grid item xs={3} >
+              <Button  type="submit" fullWidth  variant="contained"  color="secondary"  className={classes.submit} > Delete account </Button> 
+              </Grid> </Grid>
               </Paper>
             </Grid>
-            {/* SCore review */}
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}>
                 <Score1 />
               </Paper>
             </Grid>
-            {/* My leding cart */}
             <Grid item xs={12}>
               <Paper className={classes.paper}>
-                <ListItemText primary="Borrowing cart" />
-                <Title1 />
+                <Title1>Reviews</Title1>
+                <br></br>
+                <Table1 />
               </Paper>
             </Grid>
           </Grid>
@@ -173,3 +189,177 @@ export default function Myaccount() {
     </div>
   );
 }
+
+// import React from 'react';
+// import clsx from 'clsx';
+// import { makeStyles } from '@material-ui/core/styles';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import Drawer from '@material-ui/core/Drawer';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import List from '@material-ui/core/List';
+// import Divider from '@material-ui/core/Divider';
+// import IconButton from '@material-ui/core/IconButton';
+// import Container from '@material-ui/core/Container';
+// import Grid from '@material-ui/core/Grid';
+// import Paper from '@material-ui/core/Paper';
+// import Link from '@material-ui/core/Link';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import {VerticalNav1} from '../../components/vertical-navs/VerticalNav1';
+// import Score1 from '../../components/features/Score1';
+// import Title1 from '../../components/features/Title1';
+// import ListItemText from '@material-ui/core/ListItemText';
+
+// const drawerWidth = 240;
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//   },
+//   toolbar: {
+//     paddingRight: 24, // keep right padding when drawer closed
+//   },
+//   toolbarIcon: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'flex-end',
+//     padding: '0 8px',
+//     ...theme.mixins.toolbar,
+//   },
+//   appBar: {
+//     zIndex: theme.zIndex.drawer + 1,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//   },
+//   appBarShift: {
+//     marginLeft: drawerWidth,
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   menuButton: {
+//     marginRight: 36,
+//   },
+//   menuButtonHidden: {
+//     display: 'none',
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+//   drawerPaper: {
+//     position: 'relative',
+//     whiteSpace: 'nowrap',
+//     width: drawerWidth,
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   drawerPaperClose: {
+//     overflowX: 'hidden',
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     width: theme.spacing(7),
+//     [theme.breakpoints.up('sm')]: {
+//       width: theme.spacing(9),
+//     },
+//   },
+//   appBarSpacer: theme.mixins.toolbar,
+//   content: {
+//     flexGrow: 1,
+//     height: '100vh',
+//     overflow: 'auto',
+//   },
+//   container: {
+//     paddingTop: theme.spacing(4),
+//     paddingBottom: theme.spacing(4),
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     display: 'flex',
+//     overflow: 'auto',
+//     flexDirection: 'column',
+//   },
+//   fixedHeight: {
+//     height: 240,
+//   },
+// }));
+
+// export default function Myaccount() {
+//   const classes = useStyles();
+//   const [open, setOpen] = React.useState(true);
+//   const handleDrawerOpen = () => {
+//     setOpen(true);
+//   };
+//   const handleDrawerClose = () => {
+//     setOpen(false);
+//   };
+//   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+//   return (
+//     <div className={classes.root}>
+//       <CssBaseline />
+//       <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+//         <Toolbar className={classes.toolbar}>
+//           <IconButton
+//             edge="start"
+//             color="inherit"
+//             aria-label="open drawer"
+//             onClick={handleDrawerOpen}
+//             className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//         </Toolbar>
+//       </AppBar>
+//       <Drawer
+//         variant="permanent"
+//         classes={{
+//           paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+//         }}
+//         open={open}
+//       >
+//         <div className={classes.toolbarIcon}>
+//           <IconButton onClick={handleDrawerClose}>
+//             <ChevronLeftIcon />
+//           </IconButton>
+//         </div>
+//         <Divider />
+//         <List>{VerticalNav1}</List>
+//       </Drawer>
+//       <main className={classes.content}>
+//         <div className={classes.appBarSpacer} />
+//         <Container maxWidth="lg" className={classes.container}>
+//           <Grid container spacing={3}>
+//             {/* JAKIS ELEMENT ? pomysły*/}
+//             <Grid item xs={12} md={8} lg={9}>
+//               <Paper className={fixedHeightPaper}>
+            
+//               </Paper>
+//             </Grid>
+//             {/* SCore review */}
+//             <Grid item xs={12} md={4} lg={3}>
+//               <Paper className={fixedHeightPaper}>
+//                 <Score1 />
+//               </Paper>
+//             </Grid>
+//             {/* My leding cart */}
+//             <Grid item xs={12}>
+//               <Paper className={classes.paper}>
+//                 <ListItemText primary="Borrowing cart" />
+//                 <Title1 />
+//               </Paper>
+//             </Grid>
+//           </Grid>
+//         </Container>
+//       </main>
+//     </div>
+//   );
+// }
